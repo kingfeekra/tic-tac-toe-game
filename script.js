4ad8bca625f15e7233e3add3f0afb07bddd6e057
kingfeekra <fiachraking@gmail.com>
2022-07-21 20:41:01 +0100

    create function that creates gameboard

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const gameBoard = (() => {
     const createBoard = function() {
-        
-    } 
+        const boardRows = document.querySelectorAll(".boardRow");
+        const squares = [];
+        for(let i = 0; i < 3; i++) {
+            for(let i = 0; i < 3; i++){
+            squares[i] = document.createElement("div");
+            squares[i].classList.add("square");
+            boardRows[i].appendChild(squares[i]);
+            }
+            
+        }
 
+    } 
+    return {
+        createBoard: function() {
+            createBoard();
+        }
+    }
     
-})
+})()
+
+gameBoard.createBoard();
 
 const player = (name) => {
     return{name};
