6da031a10274751afb82a9d244260b63fd0fc4ec
kingfeekra <fiachraking@gmail.com>
2022-07-21 19:33:22 +0100

    Create divs in HTML file for board display
    some styles added
    
    player object created in JS file

---
+++ script.js
@@ -0,0 +1,14 @@
+const gameBoard = (() => {
+    const createBoard = function() {
+        
+    } 
+
+    
+})
+
+const player = (name) => {
+    return{name};
+};
+
+const player1 = player("player1");
+const player2 = player("player2");
